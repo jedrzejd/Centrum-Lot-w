e94a36e27981fab2ddd44119c0423afd85c9b679
Jędrzej Dudzicz <jedrzejdudzicz@gmail.com>
2021-06-07 21:10:11 +0200

    poprawienie skladni

--- tests/test_samolot.py
+++ tests/test_samolot.py
@@ -1,18 +1,21 @@
 from samolot import Samolot
 
+
 def test_create_samolot():
-    samolot = Samolot(150.0,'UHC64920',75)
+    samolot = Samolot(150.0, 'UHC64920', 75)
 
     assert isinstance(samolot,Samolot)
 
+
 def test_getZasieg_samolot():
-    samolot = Samolot(150.0,'UHC64920',75)
+    samolot = Samolot(150.0, 'UHC64920', 75)
 
     zasieg_samolot = samolot.getZasieg()
 
     assert isinstance(samolot, Samolot)
     assert isinstance(zasieg_samolot, float)
     assert zasieg_samolot == 150.0
+
 
 def test_getId_samolot():
     samolot = Samolot(150.0, 'UHC64920', 75)
@@ -22,6 +25,7 @@ def test_getId_samolot():
     assert isinstance(samolot, Samolot)
     assert isinstance(id_samolot, str)
     assert id_samolot == 'UHC64920'
+
 
 def test_getLiczbamiejsc():
     samolot = Samolot(150.0, 'UHC64920', 75)
