fc560ac7268c04261d440be928a13ab5ce83e31d
mimiaczekpluszaczek <mimi020001@gmail.com>
2021-06-08 15:03:46 +0200

    zmiany w testach lini lotniczej finalne zmiany

--- tests/test_linialotnicza.py
+++ tests/test_linialotnicza.py
@@ -132,7 +132,6 @@ def test_getKlienci():
     assert klienci[0]==klient_1
 
 
-
 def test_dodajKlienta():
     samolot = Samolot(1500.5, 'ASI', 120)
     lotnisko_1 = Lotnisko('Japonia', 'Baku', 'FSFJU123')
@@ -153,6 +152,7 @@ def test_dodajKlienta():
 
     assert isinstance(linialotnicza,Linialotnicza)
     assert ostatniklient == 'md_987'
+
 
 def test_usunKlienta():
     samolot = Samolot(1500.5, 'ASI', 120)
@@ -175,6 +175,126 @@ def test_usunKlienta():
     assert isinstance(linialotnicza,Linialotnicza)
     assert len(klienci) == 1
     assert ostatniklient == 'jd_123'
+
+
+def test_dodaj_Samolot():
+    samolot = Samolot(1500.5, 'ASI', 120)
+    samolot_2 = Samolot(2000.1, 'BAS', 60)
+    lotnisko_1 = Lotnisko('Japonia', 'Baku', 'FSFJU123')
+    lotnisko_2 = Lotnisko('Filipiny', 'Ramzes', 'AVIZ652')
+    klient_1 = Klient('jd_123')
+    klient_2 = Klient('md_987')
+    bilet_1_1 = Bilet('Japonia', 'Filipiny', datetime(2021, 1, 2))
+    bilet_2_1 = Bilet('Jemen', 'Filipiny', datetime(2021, 1, 2))
+    rezerwacja_1 = Rezerwacja(klient_1, [bilet_1_1])
+    rezerwacja_2 = Rezerwacja(klient_2, [bilet_2_1])
+    trasa_2 = Trasa(1233.2, 12, [lotnisko_1, lotnisko_2],2)
+    lot = Lot(samolot, trasa_2, [rezerwacja_1, rezerwacja_2], 0)
+    linialotnicza = Linialotnicza('Lataj z Nami', [samolot], [lot], [trasa_2], [klient_1,klient_2])
+
+    linialotnicza.dodajSamolot(samolot_2)
+    samolot = linialotnicza.getSamoloty()
+    ostatniksamolot = samolot[-1].getId()
+
+    assert isinstance(linialotnicza,Linialotnicza)
+    assert ostatniksamolot == 'BAS'
+
+def test_dodaj_usun_Samolot():
+    samolot = Samolot(1500.5, 'ASI', 120)
+    samolot_2 = Samolot(2000.1, 'BAS', 60)
+    lotnisko_1 = Lotnisko('Japonia', 'Baku', 'FSFJU123')
+    lotnisko_2 = Lotnisko('Filipiny', 'Ramzes', 'AVIZ652')
+    klient_1 = Klient('jd_123')
+    klient_2 = Klient('md_987')
+    bilet_1_1 = Bilet('Japonia', 'Filipiny', datetime(2021, 1, 2))
+    bilet_2_1 = Bilet('Jemen', 'Filipiny', datetime(2021, 1, 2))
+    rezerwacja_1 = Rezerwacja(klient_1, [bilet_1_1])
+    rezerwacja_2 = Rezerwacja(klient_2, [bilet_2_1])
+    trasa_2 = Trasa(1233.2, 12, [lotnisko_1, lotnisko_2],2)
+    lot = Lot(samolot, trasa_2, [rezerwacja_1, rezerwacja_2], 0)
+    linialotnicza = Linialotnicza('Lataj z Nami', [samolot], [lot], [trasa_2], [klient_1,klient_2])
+
+    linialotnicza.dodajSamolot(samolot_2)
+    linialotnicza.usunSamolot(samolot)
+    samolot = linialotnicza.getSamoloty()
+    ostatniksamolot = samolot[0].getId()
+
+    assert isinstance(linialotnicza,Linialotnicza)
+    assert ostatniksamolot == 'BAS'
+
+
+def test_dodaj_Trasa():
+    samolot = Samolot(1500.5, 'ASI', 120)
+    lotnisko_1 = Lotnisko('Japonia', 'Baku', 'FSFJU123')
+    lotnisko_2 = Lotnisko('Filipiny', 'Ramzes', 'AVIZ652')
+    lotnisko_3 = Lotnisko('Jemen', 'Baku', 'ABAF1247')
+    klient_1 = Klient('jd_123')
+    klient_2 = Klient('md_987')
+    bilet_1_1 = Bilet('Japonia', 'Filipiny', datetime(2021, 1, 2))
+    bilet_2_1 = Bilet('Jemen', 'Filipiny', datetime(2021, 1, 2))
+    rezerwacja_1 = Rezerwacja(klient_1, [bilet_1_1])
+    rezerwacja_2 = Rezerwacja(klient_2, [bilet_2_1])
+    trasa_2 = Trasa(1233.2, 12, [lotnisko_1, lotnisko_2],2)
+    trasa_3 = Trasa(633.2, 7, [lotnisko_3, lotnisko_2],3)
+    lot = Lot(samolot, trasa_2, [rezerwacja_1, rezerwacja_2], 0)
+    linialotnicza = Linialotnicza('Lataj z Nami', [samolot], [lot], [trasa_2], [klient_1,klient_2])
+
+    linialotnicza.dodajTrase(trasa_3)
+    trasy = linialotnicza.getTrasy()
+    ostatniatrasa = trasy[-1].getID()
+
+    assert isinstance(linialotnicza,Linialotnicza)
+    assert ostatniatrasa == 3
+
+
+def test_dodaj_usunTrasa():
+    samolot = Samolot(1500.5, 'ASI', 120)
+    lotnisko_1 = Lotnisko('Japonia', 'Baku', 'FSFJU123')
+    lotnisko_2 = Lotnisko('Filipiny', 'Ramzes', 'AVIZ652')
+    lotnisko_3 = Lotnisko('Jemen', 'Baku', 'ABAF1247')
+    klient_1 = Klient('jd_123')
+    klient_2 = Klient('md_987')
+    bilet_1_1 = Bilet('Japonia', 'Filipiny', datetime(2021, 1, 2))
+    bilet_2_1 = Bilet('Jemen', 'Filipiny', datetime(2021, 1, 2))
+    rezerwacja_1 = Rezerwacja(klient_1, [bilet_1_1])
+    rezerwacja_2 = Rezerwacja(klient_2, [bilet_2_1])
+    trasa_2 = Trasa(1233.2, 12, [lotnisko_1, lotnisko_2],2)
+    trasa_3 = Trasa(633.2, 7, [lotnisko_3, lotnisko_2],3)
+    lot = Lot(samolot, trasa_2, [rezerwacja_1, rezerwacja_2], 0)
+    linialotnicza = Linialotnicza('Lataj z Nami', [samolot], [lot], [trasa_2], [klient_1,klient_2])
+
+    linialotnicza.dodajTrase(trasa_3)
+    linialotnicza.usunTrase(trasa_2)
+
+    trasy = linialotnicza.getTrasy()
+    ostatniatrasa = trasy[0].getID()
+
+    assert isinstance(linialotnicza,Linialotnicza)
+    assert ostatniatrasa == 3
+
+def test_generowanieLotu():
+    samolot = Samolot(1500.5, 'ASI', 120)
+    lotnisko_1 = Lotnisko('Japonia', 'Baku', 'FSFJU123')
+    lotnisko_2 = Lotnisko('Filipiny', 'Ramzes', 'AVIZ652')
+    lotnisko_3 = Lotnisko('Jemen', 'Baku', 'ABAF1247')
+    klient_1 = Klient('jd_123')
+    klient_2 = Klient('md_987')
+    bilet_1_1 = Bilet('Japonia', 'Filipiny', datetime(2021, 1, 2))
+    bilet_2_1 = Bilet('Jemen', 'Filipiny', datetime(2021, 1, 2))
+    rezerwacja_1 = Rezerwacja(klient_1, [bilet_1_1])
+    rezerwacja_2 = Rezerwacja(klient_2, [bilet_2_1])
+    trasa_2 = Trasa(1233.2, 12, [lotnisko_1, lotnisko_2], 2)
+    trasa_3 = Trasa(633.2, 7, [lotnisko_3, lotnisko_2], 3)
+    lot = Lot(samolot, trasa_2, [rezerwacja_1, rezerwacja_2], 0)
+    linialotnicza = Linialotnicza('Lataj z Nami', [samolot], [lot], [trasa_2], [klient_1, klient_2])
+
+    linialotnicza.generujLot(samolot,trasa_3,[rezerwacja_1,rezerwacja_2],1)
+
+    loty=linialotnicza.getLoty()
+    ostatnilot=loty[-1].getRodzajLotu()
+
+    assert isinstance(linialotnicza,Linialotnicza)
+    assert ostatnilot == 1
 
 
 # from linialotnicza import Linialotnicza
